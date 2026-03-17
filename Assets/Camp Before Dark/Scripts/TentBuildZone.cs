3b6796d4eb3cbc90e69c2a035c65766f31d21b7e
Rob Blofield <141638136+robblofield@users.noreply.github.com>
2026-03-17 22:21:48 +0000

    finished tent setup

---
+++ Assets/Camp Before Dark/Scripts/TentBuildZone.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class TentBuildZone : MonoBehaviour
+{
+    [Header("Poles")]
+    [SerializeField] private int requiredPoleCount = 5;
+
+    [Header("Canvas Visual")]
+    [SerializeField] private Renderer canvasRenderer;
+    [SerializeField] private Material ghostCanvasMaterial;
+    [SerializeField] private Material builtCanvasMaterial;
+
+    private int placedPoleCount = 0;
+    private bool polesComplete = false;
+    private bool canvasPlaced = false;
+    private bool tentComplete = false;
+
+    public bool PolesComplete => polesComplete;
+    public bool CanvasPlaced => canvasPlaced;
+    public bool TentComplete => tentComplete;
+
+    private void Start()
+    {
+        if (canvasRenderer != null && ghostCanvasMaterial != null)
+            canvasRenderer.material = ghostCanvasMaterial;
+    }
+
+    public void RegisterPlacedPole()
+    {
+        if (tentComplete)
+            return;
+
+        placedPoleCount++;
+
+        if (placedPoleCount >= requiredPoleCount)
+        {
+            polesComplete = true;
+            Debug.Log("All tent poles placed.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!polesComplete)
+            return;
+
+        if (canvasPlaced)
+            return;
+
+        TentCanvas canvas = other.GetComponentInParent<TentCanvas>();
+
+        if (canvas == null)
+            return;
+
+        canvasPlaced = true;
+
+        if (canvasRenderer != null && builtCanvasMaterial != null)
+            canvasRenderer.material = builtCanvasMaterial;
+
+        Destroy(canvas.gameObject);
+
+        CheckForTentComplete();
+    }
+
+    private void CheckForTentComplete()
+    {
+        if (tentComplete)
+            return;
+
+        if (polesComplete && canvasPlaced)
+        {
+            tentComplete = true;
+            Debug.Log("Tent complete.");
+        }
+    }
+}
---
+++ Assets/Camp Before Dark/Scripts/TentPoleSnapPoint.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class TentPoleSnapPoint : MonoBehaviour
+{
+    [Header("Pole Visual To Update")]
+    [SerializeField] private Renderer targetRenderer;
+    
+
+    [Header("Materials")]
+    [SerializeField] private Material ghostMaterial;
+    [SerializeField] private Material builtMaterial;
+
+    private TentBuildZone buildZone;
+    private bool isFilled = false;
+
+    private void Awake()
+    {
+        buildZone = GetComponentInParent<TentBuildZone>();
+
+        if (targetRenderer != null && ghostMaterial != null)
+        {
+            targetRenderer.material = ghostMaterial;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isFilled)
+            return;
+
+        TentPole pole = other.GetComponentInParent<TentPole>();
+
+        if (pole == null)
+            return;
+
+        isFilled = true;
+
+        // Swap material on target renderer
+        if (targetRenderer != null && builtMaterial != null)
+        {
+            targetRenderer.material = builtMaterial;
+        }
+
+        
+
+        // Prevent double trigger
+        Collider col = GetComponent<Collider>();
+        if (col != null)
+            col.enabled = false;
+
+        if (buildZone != null)
+            buildZone.RegisterPlacedPole();
+        //  Destroy held pole and turn off own renderer
+        Destroy(pole.gameObject);
+        gameObject.GetComponent<Renderer>().enabled = false;
+    }
+}
